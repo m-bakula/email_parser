e992933f0e165997a2ec7b4de206d546b92131c0
m-bakula <m.bakula@protonmail.com>
2022-08-01 17:16:24 +0200

    refactor: simplify commandworker.search(), is_in() and group_by_domain() methods

--- src/commandworker.py
+++ src/commandworker.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 
-from src import functions
+from src.functions import get_domain
 
 
 class CommandWorker:
@@ -11,18 +11,15 @@ class CommandWorker:
     @staticmethod
     def search(contents: list[str], query: str) -> list[str]:
         """Searches a list of strings (contents) for a query. Returns a list of strings that are a match"""
-        results: list[str] = []
-        for line in contents:
-            if functions.search(query, line):
-                results.append(line)
+        results: list[str] = list(filter(lambda line: (query in line), contents))
         return results
 
     @staticmethod
-    def is_in(address: str, log_contents: list[str]) -> bool:
-        """Checks if an address can be found in log_contents. Returns True or False accordingly"""
+    def is_in(query: str, contents: list[str]) -> bool:
+        """Checks if a query can be found in contents. Returns True or False accordingly"""
         found = False
-        for line in log_contents:
-            if functions.search(address, line):
+        for line in contents:
+            if query in line:
                 found = True
                 break
         return found
@@ -32,9 +29,9 @@ class CommandWorker:
         """Takes a list of email addresses and groups them by domain. Returns a list of tuples: (domain, addr_list)"""
         domain_dict: defaultdict[str, list[str]] = defaultdict(list)
         for address in sorted(contents):
-            domain_dict[functions.get_domain(address)].append(address)
-        output: list[tuple[str, list[str]]] = []
+            domain_dict[get_domain(address)].append(address)
 
+        output: list[tuple[str, list[str]]] = []
         for domain in sorted(domain_dict.keys()):
             output.append((domain, sorted(domain_dict.get(domain))))
         return output
